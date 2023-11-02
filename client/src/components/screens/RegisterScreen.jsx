cddefe3ac56a59b3536814231e6e8eaf85bb8db3
Chirag soni <129665526+chirag-soni23@users.noreply.github.com>
2023-11-02 09:36:12 +0530

    Added more files

--- client/src/components/screens/RegisterScreen.jsx
+++ client/src/components/screens/RegisterScreen.jsx
@@ -1,43 +1,47 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 
 function RegisterScreen() {
-    const [name,setname] = useState("")
-    const [email,setEmail] = useState("")
-    const [password,setPassword] = useState("")
-    const [cpassword,setcPassword] = useState("")
-    function register(){
-        if(password == cpassword){
-                const user={
-                    name,
-                    email,
-                    password,
-                    cpassword
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [cpassword, setCPassword] = useState('');
 
-                }
-                console.log(user);
+    async function register() {
+        if (password === cpassword) {
+            const user = {
+                name,
+                email,
+                password,
+                cpassword
+            };
+            try {
+                const response = await axios.post('http://localhost:5000/api/users/register', user);
+                const data = response.data;
+                // Handle the data or provide feedback to the user.
+            } catch (error) {
+                console.log(error);
+                // Handle the error or provide feedback to the user.
             }
-            else{
-                alert("Password dosen't match")
-            }
+        } else {
+            alert("Passwords don't match");
+        }
     }
-  return (
-    <div className="container">
-      <div className="row justify-content-center mt-5 ">
-        <div className="col-md-5 bs">
-          <h2>Register</h2>
-          <input type="text" className="form-control" placeholder="Enter Your name"  value={name} onChange={(e)=>{setname(e.target.value)}}/>
-          <input type="email" className="form-control" placeholder="Enter Your Email" value={email} onChange={(e)=>{setEmail(e.target.value)}} />
-          <input type="password" className="form-control" placeholder="Enter Your Password"  value={password} onChange={(e)=>{
-            setPassword(e.target.value)
-          }}/>
-          <input type="password" className="form-control" placeholder="Enter Confirm Password" value={cpassword} onChange={(e)=>{
-            setcPassword(e.target.value)
-          }} />
-          <button className='btn mt-3' onClick={register} >Register</button>
+
+    return (
+        <div className="container">
+            <div className="row justify-content-center mt-5">
+                <div className="col-md-5 bs">
+                    <h2>Register</h2>
+                    <input type="text" className="form-control" placeholder="Enter Your name" value={name} onChange={(e) => setName(e.target.value)} />
+                    <input type="email" className="form-control" placeholder="Enter Your Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                    <input type="password" className="form-control" placeholder="Enter Your Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                    <input type="password" className="form-control" placeholder="Enter Confirm Password" value={cpassword} onChange={(e) => setCPassword(e.target.value)} />
+                    <button className='btn mt-3' onClick={register}>Register</button>
                 </div>
-      </div>
-    </div>
-  );
+            </div>
+        </div>
+    );
 }
 
 export default RegisterScreen;
--- server.js
+++ server.js
@@ -4,12 +4,14 @@ const app = express()
 const port = process.env.PORT || 5000
 const dbconfig = require("./db")
 const roomRoute = require("./routes/roomRoutes.js")
+const userRoute = require("./routes/usersRoutes.js")
 const cors = require("cors")
 app.use(cors())
 
 
 app.use(express.json())
 app.use("/api/rooms",roomRoute)
+app.use("/api/users",userRoute)
 app.listen(port,()=>{
     console.log(`Server listening port no. is ${port} `);
 })
