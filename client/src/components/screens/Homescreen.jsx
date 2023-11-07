5e1757a9b708ffb6e3a56ee1d233a30546696b82
Chirag soni <129665526+chirag-soni23@users.noreply.github.com>
2023-11-07 16:18:23 +0530

    Added much more filess

--- client/src/components/screens/Homescreen.jsx
+++ client/src/components/screens/Homescreen.jsx
@@ -3,11 +3,9 @@ import axios from "axios";
 import moment from "moment";
 import Rooms from "../Rooms";
 import Loader from "../Loader";
-import Error from "../Error";
 import { DatePicker, Input } from "antd";
 
 const { RangePicker } = DatePicker;
-const { Search } = Input;
 
 function Homescreen() {
   const [loading, setLoading] = useState(false);
@@ -15,8 +13,8 @@ function Homescreen() {
   const [rooms, setRooms] = useState([]);
   const [fromdate, setFromdate] = useState(null);
   const [todate, setTodate] = useState(null);
-  const [duplicaterooms, setDuplicatrooms] = useState([]);
   const [searchQuery, setSearchQuery] = useState("");
+  const [selectedRoomType, setSelectedRoomType] = useState("all"); // Initialize room type filter
   const currentDate = moment().format("DD-MM-YYYY");
 
   useEffect(() => {
@@ -27,7 +25,6 @@ function Homescreen() {
           await axios.get("http://localhost:5000/api/rooms/getallrooms")
         ).data;
         setRooms(data);
-        setDuplicatrooms(data);
         setLoading(false);
       } catch (err) {
         setError(true);
@@ -39,53 +36,71 @@ function Homescreen() {
     fetchData();
   }, []);
 
-  function filterByDate(dates, dateStrings) {
-    if (dateStrings && dateStrings.length === 2) {
-      setFromdate(dateStrings[0]);
-      setTodate(dateStrings[1]);
-    } else {
-      setFromdate(null);
-      setTodate(null);
-    }
-  }
+  const filterRooms = () => {
+    // Filter rooms based on searchQuery and selectedRoomType
+    const filteredRooms = rooms.filter((room) => {
+      const lowerCaseSearchQuery = searchQuery.toLowerCase();
+      const lowerCaseName = room.name.toLowerCase();
+      const lowerCaseDescription = room.description.toLowerCase();
 
-  function handleSearch(value) {
-    setSearchQuery(value);
-  }
+      return (
+        (lowerCaseName.includes(lowerCaseSearchQuery) ||
+          lowerCaseDescription.includes(lowerCaseSearchQuery)) &&
+        (selectedRoomType === "all" || room.type === selectedRoomType)
+      );
+    });
 
-  const filteredRooms = rooms.filter((room) => {
-    return (
-      (room.name && room.name.toLowerCase().includes(searchQuery.toLowerCase())) ||
-      (room.description && room.description.toLowerCase().includes(searchQuery.toLowerCase()))
-    );
-  });
+    return filteredRooms;
+  };
+
+  const handleSearchKeyUp = (e) => {
+    setSearchQuery(e.target.value);
+  };
+
+  const handleRoomTypeChange = (e) => {
+    setSelectedRoomType(e.target.value);
+  };
 
   return (
-    <div className="container">
-      <div className="row mt-5">
-        <div className="col-md-3">
-          <RangePicker format="DD-MM-YYYY" onChange={filterByDate} />
-        </div>
-        <div className="col-md-3">
-          <Search
-            placeholder="Search rooms"
-            onSearch={(value) => handleSearch(value)}
+    <div className="container mt-5">
+      <div className="row bs">
+        <div className="col-md-4">
+          <RangePicker
+            format="DD-MM-YYYY"
+            onChange={(dates, dateStrings) => {
+              setFromdate(dateStrings[0]);
+              setTodate(dateStrings[1]);
+            }}
+            style={{ width: "100%" }}
           />
+        </div>
+        <div className="col-md-4">
+          <Input className="input"
+            placeholder="Search rooms"
+            onKeyUp={handleSearchKeyUp}
+            style={{ width: "100%" }}
+          />
+        </div>
+        <div className="col-md-4">
+          <select
+            className="form-control"
+            value={selectedRoomType}
+            onChange={handleRoomTypeChange}
+          >
+            <option value="all">All</option>
+            <option value="Delux">Delux</option>
+            <option value="Non-Delux">Non-Delux</option>
+          </select>
         </div>
       </div>
 
       <div className="row justify-content-center mt-2">
         {loading ? (
           <Loader />
-        ) : filteredRooms.length > 0 ? (
-          filteredRooms.map((room) => (
+        ) : filterRooms().length > 0 ? (
+          filterRooms().map((room) => (
             <div key={room._id} className="col-md-9 mt-4">
-              <Rooms
-                rooms={room}
-                fromdate={fromdate}
-                todate={todate}
-                currentDate={currentDate}
-              />
+              <Rooms rooms={room} fromdate={fromdate} todate={todate} currentDate={currentDate} />
             </div>
           ))
         ) : (
@@ -97,3 +112,4 @@ function Homescreen() {
 }
 
 export default Homescreen;
+
