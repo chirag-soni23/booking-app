0ff880fa94c734f25aa990b70c351bff7e431e28
Chirag soni <129665526+chirag-soni23@users.noreply.github.com>
2023-11-07 22:27:50 +0530

    Commit

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -1,42 +1,50 @@
-const express = require('express')
+const express = require('express');
 const router = express.Router();
-const User = require("../models/user")
-router.post("/register", async (req, res) => {
-    const newuser = new User({ name: req.body.name, email: req.body.email, password: req.body.password })
-    try {
-        const user = await newuser.save()
-        res.send("User Registered Successfully")
+const bcrypt = require('bcrypt');
+const User = require('../models/user');
 
+router.post('/register', async (req, res) => {
+  const { name, email, password } = req.body;
 
-    } catch (error) {
-        return res.status(400).json({ error })
+  try {
+    // Hash the user's password before saving it to the database
+    const hashedPassword = await bcrypt.hash(password, 10); // You can adjust the number of salt rounds
 
-    }
-
+    const newUser = new User({ name, email, password: hashedPassword });
+    const user = await newUser.save();
+    res.send('User Registered Successfully');
+  } catch (error) {
+    return res.status(400).json({ error });
+  }
 });
-router.post("/login", async (req, res) => {
-    const { email, password } = req.body
-    try {
 
-        const user = await User.findOne({ email: email, password: password })
-        if (user) {
-            const temp = {
-                name: user.name,
-                email: user.email,
-                isAdmin: user.isAdmin,
-                _id: user._id
+router.post('/login', async (req, res) => {
+  const { email, password } = req.body;
 
-            }
-            res.send(temp)
-        }
-        else {
-            return res.status(400).json({ message: "Login failed" })
+  try {
+    const user = await User.findOne({ email });
 
-        }
-    } catch (error) {
-        return res.status(400).json({ error })
+    if (user) {
+      // Verify the password using bcrypt
+      const isPasswordValid = await bcrypt.compare(password, user.password);
 
-
+      if (isPasswordValid) {
+        const temp = {
+          name: user.name,
+          email: user.email,
+          isAdmin: user.isAdmin,
+          _id: user._id,
+        };
+        res.send(temp);
+      } else {
+        return res.status(400).json({ message: 'Login failed' });
+      }
+    } else {
+      return res.status(400).json({ message: 'Login failed' });
     }
-})
+  } catch (error) {
+    return res.status(400).json({ error });
+  }
+});
+
 module.exports = router;
