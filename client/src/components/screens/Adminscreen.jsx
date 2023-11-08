75c87d5793496af4b3f5f6c18b1efcac8015040f
Chirag soni <129665526+chirag-soni23@users.noreply.github.com>
2023-11-08 09:20:10 +0530

    Added much more files

--- client/src/components/screens/Adminscreen.jsx
+++ client/src/components/screens/Adminscreen.jsx
@@ -26,7 +26,7 @@ function Adminscreen() {
           <Rooms />
         </TabPane>
         <TabPane tab="Add Room" key="3">
-          <h1>Add room</h1>
+        <Addroom/>
         </TabPane>
         <TabPane tab="Users" key="4">
           <Users />
@@ -37,6 +37,8 @@ function Adminscreen() {
 }
 
 export default Adminscreen;
+
+// Booking List Compnents
 
 export function Bookings() {
   const [bookings, setbookings] = useState([]);
@@ -97,7 +99,7 @@ export function Bookings() {
     </div>
   );
 }
-
+// Rooms List components
 export function Rooms() {
   const [rooms, setrooms] = useState([]);
   const [loading, setLoading] = useState(true);
@@ -157,7 +159,7 @@ export function Rooms() {
     </div>
   );
 }
-
+// Users List Components
 export function Users() {
   const [users, setusers] = useState([]);
   const [loading, setLoading] = useState(true);
@@ -213,3 +215,82 @@ export function Users() {
     </div>
   );
 }
+
+// Add Rooms Components
+
+export function Addroom(){
+  const [name,setname] = useState("")
+  const [rentperday,setrentperday] = useState()
+  const [maxcount,setmaxcount] = useState()
+  const[description,setdescription] = useState()
+  const[phonenumber,setphonenumber] = useState()
+  const [type,settype] = useState()
+  const [imageurl1,setimageurl1] = useState()
+  const [imageurl2,setimageurl2] = useState()
+  const [imageurl3,setimageurl3] = useState()
+  function Addroom(){
+    const newroom ={
+      name,
+      rentperday,
+      maxcount,
+      description,
+      phonenumber,
+      type,
+      imageurls:[imageurl1,imageurl2,imageurl3]
+    }
+    setname("")
+    setrentperday("")
+    setmaxcount("")
+    setdescription("")
+    setphonenumber("")
+    settype("")
+    setimageurl1("")
+    setimageurl2("")
+    setimageurl3("")
+    console.log(newroom);
+  }
+
+  return(
+    <div className="row">
+      <div className="col-md-5" style={{display:"flex",flexDirection:"column",gap:"20px"}}>
+        <input type="text" value={name} onChange={(e)=>{
+          setname(e.target.value)
+        }}  className="form-control" placeholder="Room name"/>
+        <input value={rentperday} onChange={(e)=>{
+          setrentperday(e.target.value)
+        }} type="text" className="form-control" placeholder="Rent Per Day"/>
+        <input value={maxcount} onChange={(e)=>{
+          setmaxcount(e.target.value)
+        }} type="text" className="form-control" placeholder="Max Count"/>
+        {/* <input type="text" className="form-control" placeholder="Description"/> */}
+        <textarea value={description} onChange={(e)=>{
+          setdescription(e.target.value)
+        }} style={{resize:"none"}} className = "form-control" cols="70" placeholder="Description" rows="10"></textarea>
+        <input value={phonenumber} onChange={(e)=>{
+          setphonenumber(e.target.value)
+        }}  type="text" className="form-control" placeholder="Phone number"/>
+        
+      </div>
+      <div className="col-md-5"style={{display:"flex",flexDirection:"column",gap:"20px"}}>
+      <input value={type} onChange={(e)=>{
+        settype(e.target.value)
+      }} type="text" className="form-control" placeholder="Type"/>
+        <input value={imageurl1} onChange={(e)=>{
+          setimageurl1(e.target.value)
+        }} type="text" className="form-control" placeholder="Image URL 1"/>
+        <input value={imageurl2} onChange={(e)=>{
+          setimageurl2(e.target.value)
+        }} type="text" className="form-control" placeholder="Image URL 2"/>
+        <input value={imageurl3} onChange={(e)=>{
+          setimageurl3(e.target.value)
+        }} type="text" className="form-control" placeholder="Image URL 3"/>
+        <div className="text-right">
+          <button className="btn mt-2" onClick={Addroom}>Add Room</button>
+        </div>
+
+      </div>
+
+    </div>
+  )
+}
+
